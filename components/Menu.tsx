4003908a442e657dc65572f9409ec20a28adb124
tetsuyanh <tetsuyanh@gmail.com>
2021-06-10 00:49:04 +0900

    update news section

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,21 +1,21 @@
 const Menu = () => {
   return (
-    <div className="flex p-4 sm:p-6 md:p-8 lg:p-8 2xl:p-12 justify-center space-x-8 sm:space-x-12 lg:space-x-16">
+    <div className="flex mb-12 justify-center space-x-8 sm:space-x-12 lg:space-x-16">
       <a
         href="#news"
-        className="text-corporate-font text-sm md:text-md lg:text-lg hover:underline rounded-full"
+        className="text-corporate-font text-sm md:text-md lg:text-lg tracking-widest"
       >
         NEWS
       </a>
       <a
         href="#service"
-        className="text-corporate-font text-sm md:text-md lg:text-lg rounded-full"
+        className="text-corporate-font text-sm md:text-md lg:text-lg tracking-widest"
       >
         SERVICE
       </a>
       <a
         href="#about"
-        className="text-corporate-font text-sm md:text-md lg:text-lg rounded-full"
+        className="text-corporate-font text-sm md:text-md lg:text-lg tracking-widest"
       >
         ABOUT
       </a>
---
+++ components/NewsCard.tsx
@@ -0,0 +1,24 @@
+const NewsCard = (props) => {
+  const descriptionMax = 60;
+  const descriptionShort =
+    props.description.length < descriptionMax
+      ? props.description
+      : props.description.substr(0, descriptionMax) + '...';
+  return (
+    <a href="" className="border rounded-lg overflow-hidden shadow-md">
+      <div className="relative mb-4">
+        <img src={props.thumbnail} alt={`${props.title}`} />
+        <h3 className="absolute bottom-0 w-full bg-white bg-opacity-75 px-4 py-2 text-lg font-bold">
+          {props.title}
+        </h3>
+      </div>
+      <div className="mx-4 mb-2">
+        <p className="text-sm mb-1">{descriptionShort}</p>
+        {/* TODO: 日付の型や文字列が決まってから動的にする */}
+        <p className="text-xs text-right mb-4">2021/06/29</p>
+      </div>
+    </a>
+  );
+};
+
+export default NewsCard;
